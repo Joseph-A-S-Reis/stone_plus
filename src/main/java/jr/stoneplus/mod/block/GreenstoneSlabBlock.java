84269bb6dac7f99235680f102100fef3b02eda2e
Joseph-A-S-Reis <josephasreis2511@gmail.com>
2025-10-28 09:15:14 -0300

    Release 0.0.2

---
+++ src/main/java/jr/stoneplus/mod/block/GreenstoneSlabBlock.java
@@ -0,0 +1,20 @@
+package jr.stoneplus.mod.block;
+
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.core.BlockPos;
+
+public class GreenstoneSlabBlock extends SlabBlock {
+	public GreenstoneSlabBlock() {
+		super(BlockBehaviour.Properties.of().sound(SoundType.STONE).strength(1.8f, 6f).requiresCorrectToolForDrops().instrument(NoteBlockInstrument.BASEDRUM));
+	}
+
+	@Override
+	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+		return 0;
+	}
+}
